009720be760534bfd96f64be4091116f82c6e003
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2023-10-04 11:10:39 +0300

    Fixed output inconsistency

--- cmake-scripts/macos-environment.cmake
+++ cmake-scripts/macos-environment.cmake
@@ -35,7 +35,7 @@ if (CMAKE_HOST_SYSTEM_NAME MATCHES "Darwin")
     message(FATAL "Could not find build brew setup")
   endif()
 
-  message("Using brew environment at ${BREW_PREFIX}")
+  message(STATUS "Using brew environment at ${BREW_PREFIX}")
   # https://github.com/actions/virtual-environments/blob/main/images/macos/macos-10.15-Readme.me
   set(OPENSSL_ROOT_DIR "${BREW_PREFIX}/opt/openssl@3")
   set(CMAKE_PREFIX_PATH "${BREW_PREFIX}")
