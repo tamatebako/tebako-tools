7e9779c275a06b89d58caa383076ba6c1a440204
Maxim Samsonov <maxirmx@sw.consulting>
2025-03-09 19:10:46 +0300

    feat: create development containers on demand

---
+++ docker/tools/tools.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Copyright (c) 2024-2025 [Ribose Inc](https://www.ribose.com).
+# All rights reserved.
+# This file is a part of tamatebako
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+set -o errexit -o pipefail -o noclobber -o nounset
+
+: "${LOCAL_BUILDS:=/tmp/tebako}"
+: "${CMAKE_VERSION:=3.24.4-1}"
+: "${RUBY_VERSION:=3.2.6}"
+: "${RUBY_INSTALL_VERSION:=0.9.3}"
+: "${ARCH:=x64}"
+
+install_cmake() {
+  echo "Running install_cmake for CMake version ${CMAKE_VERSION} for ${ARCH}"
+  local cmake_install="${LOCAL_BUILDS}/cmake"
+  mkdir -p "${cmake_install}"
+  pushd "${cmake_install}"
+  wget -nv "https://github.com/xpack-dev-tools/cmake-xpack/releases/download/v${CMAKE_VERSION}/xpack-cmake-${CMAKE_VERSION}-linux-${ARCH}.tar.gz"
+  tar -zxf "xpack-cmake-${CMAKE_VERSION}-linux-${ARCH}.tar.gz" --directory /usr --strip-components=1 --skip-old-files
+  popd
+  rm -rf "${cmake_install}"
+}
+
+install_ruby() {
+  echo "Running ruby_install version ${RUBY_INSTALL_VERSION} for Ruby ${RUBY_VERSION}"
+  local ruby_install=${LOCAL_BUILDS}/ruby_install
+  mkdir -p "${ruby_install}"
+  pushd "${ruby_install}"
+  wget -nv "https://github.com/postmodern/ruby-install/releases/download/v${RUBY_INSTALL_VERSION}/ruby-install-${RUBY_INSTALL_VERSION}.tar.gz"
+  tar -xzvf "ruby-install-${RUBY_INSTALL_VERSION}.tar.gz"
+  cd "ruby-install-${RUBY_INSTALL_VERSION}"
+  make install
+  ruby-install --system ruby "${RUBY_VERSION}" -- --without-gmp --disable-dtrace --disable-debug-env --disable-install-doc CC="${CC}"
+  popd
+  rm -rf "${ruby_install}"
+}
+
+DIR0=$( dirname "$0" )
+DIR_TOOLS=$( cd "$DIR0" && pwd )
+
+echo "Running tools.sh with args: $* DIR_TOOLS: ${DIR_TOOLS}"
+
+"$@"
---
+++ docker/ubuntu-20.04-dev.Dockerfile
@@ -0,0 +1,50 @@
+# Copyright (c) 2024-2025 [Ribose Inc](https://www.ribose.com).
+# All rights reserved.
+# This file is a part of tamatebako
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+FROM ubuntu:focal
+
+ENV DEBIAN_FRONTEND=noninteractive
+ENV TZ=Etc/UTC
+ARG ARCH=x64
+
+RUN apt-get -y update && \
+    apt-get -y install sudo wget git make pkg-config clang-12 clang++-12      \
+    autoconf binutils-dev libevent-dev acl-dev libfmt-dev libjemalloc-dev     \
+    libdouble-conversion-dev libiberty-dev liblz4-dev liblzma-dev libssl-dev  \
+    libboost-filesystem-dev libboost-program-options-dev libboost-system-dev  \
+    libboost-iostreams-dev  libboost-date-time-dev libboost-context-dev       \
+    libboost-regex-dev libboost-thread-dev libbrotli-dev libunwind-dev        \
+    libdwarf-dev libelf-dev libgoogle-glog-dev libffi-dev libgdbm-dev         \
+    libyaml-dev libncurses-dev libreadline-dev libutfcpp-dev libstdc++-10-dev \
+    gcc-10 g++-10 ruby
+
+ENV CC=clang-12
+ENV CXX=clang++-12
+
+COPY tools /opt/tools
+RUN /opt/tools/tools.sh install_cmake
+
+ENV PS1="\[\]\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ \[\]"
+CMD ["bash"]
